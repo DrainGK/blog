b12a17a11855f37b8bf19793b1c51e949448a1a1
Guillaume Lopez <guillaumelopezgk@hotmail.com>
2023-05-07 20:51:17 +0900

    Blog started and finished in a week from my working computer

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.SECRET_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to mongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("file has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/post", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => [console.log("Backend is running")]);
